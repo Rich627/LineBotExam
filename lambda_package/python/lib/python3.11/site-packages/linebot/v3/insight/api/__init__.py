56889334a69682e66bdd1d6f3683415176f41008
Rich627 <103445968+Rich627@users.noreply.github.com>
2024-02-07 16:57:32 +0800

    Delete lambda_package directory

--- lambda_package/python/lib/python3.11/site-packages/linebot/v3/insight/api/__init__.py
+++
@@ -1,9 +0,0 @@
-# flake8: noqa
-
-# import apis into api package
-from linebot.v3.insight.api.insight import Insight
-
-
-# Async version
-from linebot.v3.insight.api.async_insight import AsyncInsight
-
--- lambda_package/python/lib/python3.11/site-packages/linebot/v3/module/api/__init__.py
+++
@@ -1,9 +0,0 @@
-# flake8: noqa
-
-# import apis into api package
-from linebot.v3.module.api.line_module import LineModule
-
-
-# Async version
-from linebot.v3.module.api.async_line_module import AsyncLineModule
-
